7dff529864ebf19b25a26dfe9abd7ca84527e392
haikalpratama-ALT <haikalpratama@mail.ugm.ac.id>
2025-09-30 13:53:12 +0700

    Add files via upload

---
+++ Homework C.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+// Fungsi untuk menghitung Fibonacci ke-n
+int fibonacci(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+
+    int f1 = 1, f2 = 1, f3;
+    for (int i = 3; i <= n; i++) {
+        f3 = f1 + f2;
+        f1 = f2;
+        f2 = f3;
+    }
+    return f2;
+}
+
+int main() {
+    const int N = 6; 
+    int n;
+
+    ifstream file("data_x.txt");
+
+    if (file) {
+        cout << "File sukses dibuka!" << endl;
+        cout << "n" << setw(5) << "y" << endl;
+
+        for (int i = 0; i < N; i++) {
+            file >> n;                
+            int y = fibonacci(n);  
+            cout << n << setw(5) << y << endl;
+        }
+
+        file.close();
+        return 0;
+    } else {
+        cout << "File gagal dibuka!" << endl;
+        return 1;
+    }
+}
+
